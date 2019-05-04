7916f40b3ca89a3d771f15415eb4dac347c1113f
OBrian16 <briwell16@gmail.com>
2019-05-04 18:12:20 +0300

    adds props details

---
+++ src/components/resumeData.js
@@ -0,0 +1,31 @@
+let resumeData = {
+    "imagebaseurl": "http://localhost:3000/",
+    "name": "Brian Ogwel",
+    "role": "Full Stack Developer",
+    "linkedinId": "Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks": [
+        {
+            "name": "linkedin",
+            "url": "https://www.linkedin.com/in/brian-ogwel-11381a153/",
+            "className": "fa fa-linkedin"
+        },
+        {
+            "name": "github",
+            "url": "https://github.com/OBrian16/",
+            "className": "fa fa-github"
+        },
+        {
+            "name": "twitter",
+            "url": "https://twitter.com/brian_ogwel",
+            "className": "fa fa-twitter"
+        },
+        {
+            "name": "skype",
+            "url": "http://skype.com",
+            "className": "fa fa-skype"
+        }
+    ],
+}
+export default resumeData
