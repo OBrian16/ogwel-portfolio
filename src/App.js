81cbebfa8c28e9e94a93ce97a8dad9b2173726c7
OBrian16 <briwell16@gmail.com>
2019-05-04 16:07:35 +0300

    adds routes to app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/header';
+import About from './components/about/about';
+import Resume from './components/resume/resume';
+import Works from './components/works/works';
+import Testimonials from './components/testimonials/testimonials'
+import Contact from './components/contact/contact'
+import Footer from './components/footer/footer';
 
 
 class App extends Component {
@@ -9,6 +15,12 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <About />
+        <Resume /> 
+        <Works/> 
+        <Testimonials />
+        <Contact />
+        <Footer />
       </div>
     );
   }
